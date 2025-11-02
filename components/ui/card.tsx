fed2f080aaef5868eaa41b6df9629ecb9babaf85
simonebrusco <simonebrusco@gmail.com>
2025-11-02 10:15:01 -0300

    Create card.tsx

---
+++ components/ui/card.tsx
@@ -0,0 +1,4 @@
+// components/ui/card.tsx
+// Shim de compatibilidade: permite importar com "@/components/ui/card"
+// mesmo tendo a implementação principal em "@/components/ui/Card".
+export { default as default, Card, CardHeader, CardTitle, CardContent, CardFooter } from './Card'
