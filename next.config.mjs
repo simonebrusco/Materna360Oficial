e51b397a2fe70b057c6307d5b64aa7933c37fecc
simonebrusco <simonebrusco@gmail.com>
2025-11-03 08:55:25 +0000

    chore(build): finalize rebase; Next eslint ignore + cdn.builder.io images

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {},
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.builder.io',
-        pathname: '/api/v1/image/**',
-      },
-    ],
-  },
+  eslint: { ignoreDuringBuilds: true },
+  images: { remotePatterns: [{ protocol: 'https', hostname: 'cdn.builder.io' }] },
 };
-
 export default nextConfig;
