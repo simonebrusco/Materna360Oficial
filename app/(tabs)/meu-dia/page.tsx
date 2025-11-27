29d389a895868a79cd27e3c0f8ec9c585ee34cf9
simonebrusco <simonebrusco@gmail.com>
2025-11-27 14:10:39 -0300

    Update page.tsx

--- app/(tabs)/meu-dia/page.tsx
+++ app/(tabs)/meu-dia/page.tsx
@@ -1,12 +1,9 @@
-import MeuDiaClient from './Client'
+import MeuDiaClient from './Client';
 
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default function Page() {
-  return (
-    <main className="materna360-premium-bg min-h-screen">
-      <MeuDiaClient />
-    </main>
-  )
+  // O <main> e o fundo já estão dentro do MeuDiaClient
+  return <MeuDiaClient />;
 }
