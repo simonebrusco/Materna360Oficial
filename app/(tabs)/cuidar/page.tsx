9f5b93c00dd2d900bf008d277bf5186a017d7be9
simonebrusco <simonebrusco@gmail.com>
2025-12-07 08:14:59 -0300

    Update page.tsx

--- app/(tabs)/cuidar/page.tsx
+++ app/(tabs)/cuidar/page.tsx
@@ -1,22 +1,5 @@
-import { unstable_noStore as noStore } from 'next/cache'
-import CuidarClient from './Client'
-import HealthyRecipesSection from '@/components/recipes/HealthyRecipesSection'
+import { redirect } from 'next/navigation'
 
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
-export default async function Page() {
-  // Avoid RSC payload caching for this route
-  noStore()
-
-  const recipesSection = <HealthyRecipesSection />
-
-  return (
-    <main
-      data-layout="page-template-v1"
-      className="bg-[var(--color-page-bg)] min-h-[100dvh] pb-24"
-    >
-      <CuidarClient recipesSection={recipesSection} />
-    </main>
-  )
+export default function CuidarRedirectPage() {
+  redirect('/meu-dia')
 }
