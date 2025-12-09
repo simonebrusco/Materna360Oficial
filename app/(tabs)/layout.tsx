3875999c1f19e4ce82d3f486589c21bf4980491d
simonebrusco <simonebrusco@gmail.com>
2025-12-09 12:39:28 -0300

    Update layout.tsx

--- app/(tabs)/layout.tsx
+++ app/(tabs)/layout.tsx
@@ -6,9 +6,23 @@ export const revalidate = 0;
 
 export default function TabsLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative" data-layout="page-template-v1">
+    <div
+      className="
+        relative
+        min-h-[100dvh]
+        flex
+        flex-col
+        bg-white
+      "
+      data-layout="page-template-v1"
+    >
       <GlobalHeader />
-      <div className="pb-24">{children}</div>
+
+      {/* área principal onde as páginas aplicam o degradê próprio */}
+      <div className="flex-1 pb-24">
+        {children}
+      </div>
+
       <BottomNav />
     </div>
   );
