125cd2db4638f3c443d9135fafd857d66620075c
simonebrusco <simonebrusco@gmail.com>
2025-12-14 09:08:26 -0300

    Update WeeklyPlannerShell.tsx

--- components/planner/WeeklyPlannerShell.tsx
+++ components/planner/WeeklyPlannerShell.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as React from 'react'
-import WeeklyPlannerCore from './WeeklyPlannerCore'
+import { WeeklyPlannerCore } from './WeeklyPlannerCore'
 
 export default function WeeklyPlannerShell() {
   return <WeeklyPlannerCore />
