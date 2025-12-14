741d7483e7735151b862399c0410177c22098c47
simonebrusco <simonebrusco@gmail.com>
2025-12-14 09:53:00 -0300

    Update WeeklyPlannerShell.tsx

--- components/planner/WeeklyPlannerShell.tsx
+++ components/planner/WeeklyPlannerShell.tsx
@@ -1,8 +1,24 @@
 'use client'
 
-import * as React from 'react'
+import React from 'react'
 import WeeklyPlannerCore from './WeeklyPlannerCore'
 
+/**
+ * WeeklyPlannerShell
+ * -------------------
+ * Camada de orquestração do planner.
+ *
+ * ❗ Regras importantes:
+ * - NÃO conter lógica de estado
+ * - NÃO conter hooks
+ * - NÃO duplicar responsabilidades do Core
+ * - Serve apenas como ponto estável de montagem
+ *
+ * Isso garante:
+ * - Estabilidade arquitetural
+ * - Facilidade de refatoração futura
+ * - Zero regressão no Meu Dia
+ */
 export default function WeeklyPlannerShell() {
   return <WeeklyPlannerCore />
 }
