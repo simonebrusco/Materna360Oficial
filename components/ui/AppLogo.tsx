9a6273579b10060543e96d5a0454b5531e877ed7
simonebrusco <simonebrusco@gmail.com>
2025-12-15 20:20:24 -0300

    Update AppLogo.tsx

--- components/ui/AppLogo.tsx
+++ components/ui/AppLogo.tsx
@@ -3,19 +3,25 @@
 import React from 'react'
 import Image from 'next/image'
 
+type Variant = 'white' | 'default'
+
 interface AppLogoProps extends React.HTMLAttributes<HTMLDivElement> {
   width?: number
   height?: number
   priority?: boolean
+  variant?: Variant
 }
 
 export function AppLogo({
   width = 160,
   height = 40,
   priority = false,
+  variant = 'white',
   className = '',
   ...props
 }: AppLogoProps) {
+  const src = variant === 'white' ? '/images/LogoBranco.png' : '/images/logo-principal.png'
+
   return (
     <div
       className={`flex items-center shrink-0 ${className}`}
@@ -23,7 +29,7 @@ export function AppLogo({
       {...props}
     >
       <Image
-        src="/images/logo-principal.png"
+        src={src}
         alt="Materna360"
         width={width}
         height={height}
