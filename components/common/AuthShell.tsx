82950223c33dbd12fe0a6bf6be914253d588a7ab
simonebrusco <simonebrusco@gmail.com>
2025-12-27 10:37:05 -0300

    Update AuthShell.tsx

--- components/common/AuthShell.tsx
+++ components/common/AuthShell.tsx
@@ -1,33 +1,17 @@
 'use client'
 
 import Image from 'next/image'
-import LegalFooter from './LegalFooter'
+import LegalFooter from '@/components/common/LegalFooter'
 
-export default function AuthShell({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function AuthShell({ children }: { children: React.ReactNode }) {
   return (
     <main className="min-h-screen w-full bg-[linear-gradient(to_bottom,#fd2597_0%,#fd2597_35%,#fdbed7_70%,#fff7fa_100%)] flex flex-col items-center justify-center px-4">
-      
-      {/* Logo */}
       <div className="mb-8 flex flex-col items-center">
-        <Image
-          src="/images/LogoBranco.png"
-          alt="Materna360"
-          width={160}
-          height={48}
-          priority
-        />
+        <Image src="/images/LogoBranco.png" alt="Materna360" width={180} height={56} priority />
       </div>
 
-      {/* Conteúdo */}
-      <div className="w-full max-w-[420px]">
-        {children}
-      </div>
+      <div className="w-full max-w-[420px]">{children}</div>
 
-      {/* Rodapé silencioso */}
       <div className="mt-10 opacity-70">
         <LegalFooter />
       </div>
