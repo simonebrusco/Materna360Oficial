639c11a449df90311f1bff152fc84c9bc8ee8cb1
simonebrusco <simonebrusco@gmail.com>
2025-12-27 10:21:30 -0300

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,15 +1,12 @@
 import { Suspense } from 'react'
 import SignupClient from './SignupClient'
-import { AuthShell } from '@/components/common/AuthShell'
+import AuthShell from '@/components/common/AuthShell'
 
 export const dynamic = 'force-dynamic'
 
 export default function SignupPage() {
   return (
-    <AuthShell
-      title="Criar conta"
-      subtitle="Leva poucos passos. O Materna360 funciona no seu ritmo."
-    >
+    <AuthShell>
       <Suspense fallback={null}>
         <SignupClient />
       </Suspense>
