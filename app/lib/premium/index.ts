8133904f385a9fa526a6b7aa06907714df31db30
simonebrusco <simonebrusco@gmail.com>
2025-12-29 07:55:27 -0300

    Update index.ts

--- app/lib/premium/index.ts
+++ app/lib/premium/index.ts
@@ -1,1 +1,10 @@
-
+// app/lib/premium/index.ts
+export { PREMIUM_CAPABILITIES, type PremiumCapability, isPremiumCapability } from './capabilities'
+export { PREMIUM_FLAGS } from './flags'
+export {
+  DEFAULT_ENTITLEMENTS,
+  type PremiumEntitlements,
+  type PremiumTier,
+  hasCapability,
+  getEntitlements,
+} from './entitlements'
